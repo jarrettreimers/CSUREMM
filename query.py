51f938534a4d72dbe1feb4e06dd72d59bd380cf0
jarrettreimers <67029302+jarrettreimers@users.noreply.github.com>
2024-05-30 11:28:16 -0400

    update

--- query.py
+++ query.py
@@ -2,8 +2,8 @@ import pandas as pd
 import datetime 
 
 class Query():
-  def get_datetime(self, year, month, day, hour, minute, second):
+  def get_datetime(self, year, month, day, hour, minute, second) -> datetime.datetime:
     return datetime.datetime(year, month, day, hour, minute, second)
   
-  def select_time(self, data: pd.DataFrame, start_time: datetime.datetime, end_time: datetime.datetime):
+  def select_time(self, data: pd.DataFrame, start_time: datetime.datetime, end_time: datetime.datetime) -> pd.DataFrame:
     return data.loc[data['starttime'] >= start_time].loc[data['starttime'] <= end_time]
