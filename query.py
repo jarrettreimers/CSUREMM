2d4aaaa9b65c57bde3cc1fb4e2e4cc54d443fd5f
jarrettreimers <67029302+jarrettreimers@users.noreply.github.com>
2024-05-31 09:41:32 -0400

    updates all around

--- query.py
+++ query.py
@@ -1,9 +1,19 @@
 import pandas as pd
-import datetime 
+import datetime
 
-class Query():
-  def get_datetime(self, year, month, day, hour, minute, second) -> datetime.datetime:
+
+def get_datetime(year, month, day, hour, minute, second) -> datetime.datetime:
     return datetime.datetime(year, month, day, hour, minute, second)
-  
-  def select_time(self, data: pd.DataFrame, start_time: datetime.datetime, end_time: datetime.datetime) -> pd.DataFrame:
+
+
+def select_time(data: pd.DataFrame, start_time: datetime.datetime,
+                end_time: datetime.datetime) -> pd.DataFrame:
     return data.loc[data['starttime'] >= start_time].loc[data['starttime'] <= end_time]
+
+
+def get_stations(data: pd.DataFrame) -> set:
+    return set(data['start station id']).union(set(data['end station id']))
+
+
+def select_start_station(data: pd.DataFrame, station_id: int) -> pd.DataFrame:
+    return data.loc[data['start station id'] == station_id]
--- stations.py
+++ stations.py
@@ -1,22 +1,20 @@
 import pandas as pd
 
-class Stations():
-  def __init__(self):
-    self.stations_id = {}
-    self.stations_count = {}
-    
-  def clear_data(self):
-    self.stations_id = {}
-    self.stations_count = {}
-    
-  def add_data(self, data: pd.DataFrame):
-    stations = set(data['start station id']).union(set(data['end station id']))
-      
-    for station in stations:
-      if station not in self.stations_count:
-        self.stations_count[station] = 0
-      self.stations_count[station] += len(data.loc[data['start station id'] == station])
-      self.stations_count[station] -= len(data.loc[data['end station id'] == station])
-      
-        
-    
+
+class Stations:
+    def __init__(self):
+        self.stations_id = {}
+        self.stations_count = {}
+
+    def clear_data(self):
+        self.stations_id = {}
+        self.stations_count = {}
+
+    def add_data(self, data: pd.DataFrame):
+        stations = set(data['start station id']).union(set(data['end station id']))
+
+        for station in stations:
+            if station not in self.stations_count:
+                self.stations_count[station] = 0
+            self.stations_count[station] -= len(data.loc[data['start station id'] == station])
+            self.stations_count[station] += len(data.loc[data['end station id'] == station])
