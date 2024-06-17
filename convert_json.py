657fee865182b5c87bc485065810f2f19d73f0bd
andrewyoo <yooandrew2004@gmail.com>
2024-06-17 13:25:38 -0400

    plots distance to time

--- convert_json.py
+++ convert_json.py
@@ -15,3 +15,23 @@ for station in stations:
 
 df = pd.DataFrame(flat_data)
 print(df)
+
+
+def get_filtered_stations(json_file_path):
+    with open(json_file_path, 'r') as file:
+        json_data = json.load(file)
+
+    stations = json_data['data']['stations']
+
+    flat_data = []
+    for station in stations:
+        if station['capacity'] > 12:  # Filter condition
+            flat_station = {
+                'name': station['name'],
+                'capacity': station['capacity'],
+                'short_name': station['short_name']
+            }
+            flat_data.append(flat_station)
+
+    df = pd.DataFrame(flat_data)
+    return df
